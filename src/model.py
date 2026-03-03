7484a411b27003eee8008379af3197b7da2ecb59
Aditya Thakur <thakuradityadev9336@gmail.com>
2026-03-03 14:26:32 +0530

    Modify to include training and testing

--- src/model.py
+++ src/model.py
@@ -1,11 +1,37 @@
-import matplotlib.pyplot as plt
 from data_loader import load_data
+from preprocessing import preprocess_data
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
 
 df = load_data("../data/sales_data.csv")
+df = preprocess_data(df)
 
-plt.figure()
-plt.plot( df['date'], df['sales'] )
-plt.title("Sales over Time")
-plt.xticks (rotation = 45)
-plt.tight_layout()
-plt.show()
+features = [
+    'day_of_week',
+    'month',
+    'lag_1',
+    'lag_7',
+    'rolling_mean_7'
+]
+
+X = df[features]
+Y = df['sales']
+
+split_index = int(len(df) * 0.8)
+
+X_train = X[:split_index]
+X_test = X[split_index:]
+Y_train = Y[:split_index]
+Y_test = Y[split_index:]
+
+model = LinearRegression()
+model.fit(X_train, Y_train)
+
+predictions = model.predict(X_test)
+
+mae = mean_absolute_error(Y_test, predictions)
+rmse = np.sqrt(mean_squared_error(Y_test, predictions))
+
+print("MAE: ", round(mae, 2))
+print("RMSE: ", round(rmse, 2))
